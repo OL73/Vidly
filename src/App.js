f94b2eac8abf48406e8f5d3127eba89448e63f77
Olivier <sogeek.ol@gmail.com>
2020-11-27 11:23:36 +0100

    jout login et react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.css";
 import Movies from "./components/movies";
+import { Route } from 'react-dom';
+import { Switch } from 'react-router-dom';
+import NavBar from "./components/navBar";
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <>
+      <NavBar />
+      <main className="container">
+        <Movies />
+      </main>
+    </>
   );
 }
 
